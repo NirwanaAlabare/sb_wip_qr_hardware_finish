945c40a5a800420e448cf5e53b4e4bb91bc5cf18
Rizky <rizkyaditya2106@gmail.com>
2024-05-27 13:37:57 +0700

    adjusting

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/tabicon.png') }}">
-    <title>SB WIP - FINISH LINE</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/endline.ico') }}">
+    <title>NDS QC FINISH QR</title>
 
     @include('layouts.link')
 
     @yield('custom-link')
 
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="{{ asset('alpinejs/dist/cdn.min.js') }}"></script>
 </head>
 <body>
     @include('layouts.navbar')
