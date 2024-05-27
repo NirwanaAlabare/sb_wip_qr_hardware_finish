cf44eaf0c48897418faa38a1e68aec014532db3e
Rizky <rizkyaditya2106@gmail.com>
2024-05-27 13:51:10 +0700

    adjusting

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/endline.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/finishline.ico') }}">
     <title>NDS QC FINISH QR</title>
 
     @include('layouts.link')
